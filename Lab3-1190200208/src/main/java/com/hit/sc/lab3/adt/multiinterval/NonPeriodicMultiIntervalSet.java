a5592cd720295e36073b3c46bab7f902d64517a1
someXXYY <betaXXX@gmail.com>
2026-04-09 21:15:02 +0800

    add

---
+++ Lab3-1190200208/src/main/java/com/hit/sc/lab3/adt/multiinterval/NonPeriodicMultiIntervalSet.java
@@ -0,0 +1,13 @@
+package com.hit.sc.lab3.adt.multiinterval;
+
+public class NonPeriodicMultiIntervalSet<L> extends MultiIntervalSetDecorator<L> implements MultiIntervalSet<L> {
+
+    // Constructor
+    public NonPeriodicMultiIntervalSet(MultiIntervalSet<L> multiIntervalSet) {
+        super(multiIntervalSet);
+    }
+
+    public boolean checkPeriodic() {
+        return false;
+    }
+}
